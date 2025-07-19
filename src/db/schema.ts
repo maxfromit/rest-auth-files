fb8b3da0c6af082e6c1b16d89c99be22a4d252d0
maxfromit <bigmaxcapacity@gmail.com>
2025-07-19 16:13:33 +0300

    src/db/schema update users and files table definitions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,15 @@
 import { mysqlTable, int, varchar, timestamp } from "drizzle-orm/mysql-core"
 
-export const users = mysqlTable("users", {
-  id: int("id").primaryKey().autoincrement(),
-  identifier: varchar("identifier", { length: 255 }).notNull().unique(), // email or phone
+export const usersTable = mysqlTable("users", {
+  id: varchar("id", { length: 255 }).primaryKey().unique(), // phone or email as string as in test task
   password_hash: varchar("password_hash", { length: 255 }).notNull(),
+  //id: int("id").primaryKey().autoincrement(),
+  //email: varchar("email", { length: 255 }).unique().optional(),
+  //phone: varchar("phone", { length: 20 }).unique().optional(),
   created_at: timestamp("created_at").defaultNow(),
 })
 
-export const files = mysqlTable("files", {
+export const filesTable = mysqlTable("files", {
   id: int("id").primaryKey().autoincrement(),
   user_id: int("user_id").notNull(),
   name: varchar("name", { length: 255 }).notNull(),
@@ -17,7 +19,7 @@ export const files = mysqlTable("files", {
   uploaded_at: timestamp("uploaded_at").defaultNow(),
 })
 
-export const tokens = mysqlTable("tokens", {
+export const tokensTable = mysqlTable("tokens", {
   id: int("id").primaryKey().autoincrement(),
   user_id: int("user_id").notNull(),
   refresh_token: varchar("refresh_token", { length: 255 }).notNull(),
