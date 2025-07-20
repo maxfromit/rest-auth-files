8792b4c4d08613432417da2dc7ad3c2d05149af6
maxfromit <bigmaxcapacity@gmail.com>
2025-07-21 02:20:19 +0300

    src/routes/infoRoutes remove debug log for user ID in GET route

--- src/routes/infoRoutes.ts
+++ src/routes/infoRoutes.ts
@@ -4,7 +4,6 @@ import { authMiddleware } from "../middleware/authMiddleware.js"
 const router = Router()
 
 router.get("/", authMiddleware, (req, res) => {
-  console.log("User ID from auth middleware:", req.userId)
   res.status(200).json({ id: req.userId })
 })
 
