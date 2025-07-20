90ebbbcf6bd46b1aa4020af165d4e08b8c0f06be
maxfromit <bigmaxcapacity@gmail.com>
2025-07-20 14:40:18 +0300

    src app update app.ts to include auth and info routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express"
 import cors from "cors"
+import authRoutes from "./routes/authRoutes.js"
+import infoRoutes from "./routes/infoRoutes.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/", authRoutes)
+app.use("/info", infoRoutes)
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK")
