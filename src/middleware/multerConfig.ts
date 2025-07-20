b14d92255490d5539b5b684afd9109dbb1c2d3c1
maxfromit <bigmaxcapacity@gmail.com>
2025-07-21 01:21:49 +0300

    middleware multerConfig create multer configuration for file uploads

---
+++ src/middleware/multerConfig.ts
@@ -0,0 +1,26 @@
+import multer from "multer"
+import { extname } from "path"
+import { randomUUID } from "crypto"
+
+import { getUploadDir } from "../utils/getUploadDir.js"
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    try {
+      cb(null, getUploadDir())
+    } catch (err) {
+      cb(err as Error, "")
+    }
+  },
+  filename: (req, file, cb) => {
+    try {
+      const id = req.params?.id || randomUUID() // preserve id for file updates
+      const extension = extname(file.originalname).toLowerCase()
+      cb(null, `${id}${extension}`)
+    } catch (err) {
+      cb(err as Error, "")
+    }
+  },
+})
+
+export const multerConfig = multer({ storage })
