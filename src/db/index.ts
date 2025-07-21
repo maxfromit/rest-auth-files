295cf4911472fdb8846bb559be6708c9c503d682
maxfromit <bigmaxcapacity@gmail.com>
2025-07-21 03:14:22 +0300

    src/db index update console log for database connection initialization

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
-// filepath: [index.ts](http://_vscodecontentref_/2)
 import { drizzle } from "drizzle-orm/mysql2"
 import mysql from "mysql2/promise"
 
+console.log("Initializing database connection...", process.env.MYSQL_HOST)
 const poolConnection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
