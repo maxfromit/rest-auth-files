019979b8350fa71ab3921115a72ca0553a72c98e
maxfromit <bigmaxcapacity@gmail.com>
2025-07-22 18:28:00 +0300

    routes/authRoutes update refresh token route to use constant for path

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,12 +6,13 @@ import {
   logoutController,
 } from "../controllers/authControllers.js"
 import { authMiddleware } from "../middleware/authMiddleware.js"
+import { REFRESH_TOKEN_COOKIE_PATH } from "../consts.js"
 
 const router = Router()
 
 router.post("/signup", signupController)
 router.post("/signin", signinController)
-router.post("/signin/new_token", refreshAccessTokenController)
+router.post(REFRESH_TOKEN_COOKIE_PATH, refreshAccessTokenController)
 router.post("/logout", authMiddleware, logoutController)
 
 export default router
