84f5e19ead88ba83fc625712990798481c0aff56
maxfromit <bigmaxcapacity@gmail.com>
2025-07-22 18:29:33 +0300

    src/db/scripts/getUsers update getUsers function to include ordering by user ID and token revoked status

--- src/db/scripts/getUsers.ts
+++ src/db/scripts/getUsers.ts
@@ -1,6 +1,7 @@
 import { db } from "../index.js"
 import { usersTable, tokensTable } from "../schema.js"
 import { eq } from "drizzle-orm"
+import { desc } from "drizzle-orm"
 
 async function getUsers() {
   console.log("getting users...")
@@ -10,6 +11,7 @@ async function getUsers() {
       .select()
       .from(usersTable)
       .leftJoin(tokensTable, eq(usersTable.id, tokensTable.user_id))
+      .orderBy(desc(usersTable.id), desc(tokensTable.revoked_at))
     console.log("Existing users:", usersWithTokens)
   } catch (err) {
     console.error("Error querying usersTable:", err)
