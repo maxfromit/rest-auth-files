ce6f047d2016f6f1f2f546db34f52f99fe36a9c5
maxfromit <bigmaxcapacity@gmail.com>
2025-07-30 19:43:24 +0300

    src/tests/authRoutes test updates to use structured messages for error handling and success responses and add tests with mocked db

--- src/tests/routes/authRoutes.test.ts
+++ src/tests/routes/authRoutes.test.ts
@@ -12,6 +12,9 @@ import {
 import { deleteAllUsers } from "../../db/scripts/deleteUsers.js"
 import { signup, signin } from "../utils.js"
 import type { User } from "../types.js"
+import { authMessages, errorKey, messageKey } from "../../consts/messages.js"
+import { vi } from "vitest"
+import { db } from "../../db/index.js"
 
 const testUserForSignup: User = {
   id: "signup@mail.test",
@@ -32,6 +35,34 @@ const testUserForRevocation: User = {
   password: "TestPassword4!",
 }
 
+function mockReturnsEmptyArray() {
+  return {
+    set: () => ({
+      where: () => Promise.resolve([]),
+    }),
+  } as unknown as ReturnType<typeof db.update>
+}
+
+function mockExpiredRefreshedToken() {
+  // Mock the database response to simulate an expired refresh token
+  const tokenRowWithExpiredOne = {
+    id: 112,
+    user_id: "user@expired.com",
+    refresh_token: "expired.refresh.token.string",
+    session_id: "188002dd-3eb4-4224-90d4-1b4c778e0b45",
+    expires_at: new Date(Date.now() - 1000), // expired
+    revoked_at: null,
+  }
+
+  const mockSelectChain = {
+    from: () => ({
+      where: () => Promise.resolve([tokenRowWithExpiredOne]),
+    }),
+  } as unknown as ReturnType<typeof db.select>
+
+  vi.spyOn(db, "select").mockReturnValue(mockSelectChain)
+}
+
 describe("Auth routes", () => {
   beforeAll(async () => {
     await deleteAllUsers()
@@ -40,27 +71,39 @@ describe("Auth routes", () => {
   afterAll(async () => {
     // await deleteAllUsers()
   })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   describe("Signup", () => {
     test("should fail when signup data is missing", async () => {
       const res = await request(app).post("/signup").send({})
       expect(res.status).toBe(400)
-      expect(res.body).toHaveProperty("error")
-      expect(res.body.error).toContain("Missing")
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.missingAuthData
+      )
     })
 
     test("should signup a new user", async () => {
       const res = await signup(testUserForSignup)
       expect(res.status).toBe(201)
       expect(res.body).toHaveProperty("accessToken")
-      expect(res.body).toHaveProperty("message", "User registered successfully")
+      expect(res.body).toHaveProperty(
+        messageKey,
+        authMessages.success.signupSuccessful
+      )
       expect(res.headers["set-cookie"]).toBeDefined()
     })
 
     test("should not allow signup with an existing user id", async () => {
       const res = await signup(testUserForSignup)
       expect(res.status).toBe(500)
-      expect(res.body).toHaveProperty("error")
-      expect(res.body.error).toContain("already")
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.userAlreadyExists
+      )
     })
   })
 
@@ -68,8 +111,10 @@ describe("Auth routes", () => {
     test("should fail when signin data is missing", async () => {
       const res = await request(app).post("/signin").send({})
       expect(res.status).toBe(400)
-      expect(res.body).toHaveProperty("error")
-      expect(res.body.error).toContain("Missing")
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.missingAuthData
+      )
     })
 
     test("should signin with correct credentials", async () => {
@@ -77,7 +122,10 @@ describe("Auth routes", () => {
       const res = await signin(testUserForSignin)
       expect(res.status).toBe(200)
       expect(res.body).toHaveProperty("accessToken")
-      expect(res.body).toHaveProperty("message", "Login successful")
+      expect(res.body).toHaveProperty(
+        messageKey,
+        authMessages.success.loginSuccessful
+      )
       expect(res.headers["set-cookie"]).toBeDefined()
     })
 
@@ -86,8 +134,10 @@ describe("Auth routes", () => {
         .post("/signin")
         .send({ ...testUserForSignin, password: "wrong" })
       expect(res.status).toBe(500)
-      expect(res.body).toHaveProperty("error")
-      expect(res.body.error).toContain("Invalid password")
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.invalidPassword
+      )
     })
 
     test("should not signin with non-existent user id", async () => {
@@ -95,19 +145,14 @@ describe("Auth routes", () => {
         .post("/signin")
         .send({ ...testUserForSignin, id: "wrong" })
       expect(res.status).toBe(500)
-      expect(res.body).toHaveProperty("error")
-      expect(res.body.error).toContain("Invalid user")
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.invalidUserId
+      )
     })
   })
 
   describe("Logout", () => {
-    test("should fail logout if not authorized", async () => {
-      const res = await request(app).post("/logout").send()
-      expect(res.status).toBe(401)
-      expect(res.body).toHaveProperty("error")
-      expect(res.body.error).toContain("invalid")
-    })
-
     test("should logout successfully", async () => {
       await signup(testUserForLogout)
       const signinRes = await signin(testUserForLogout)
@@ -119,7 +164,19 @@ describe("Auth routes", () => {
         .send()
 
       expect(res.status).toBe(200)
-      expect(res.body).toHaveProperty("message", "Logged out successfully")
+      expect(res.body).toHaveProperty(
+        messageKey,
+        authMessages.success.logoutSuccessful
+      )
+    })
+
+    test("should fail logout if missed accessToken", async () => {
+      const res = await request(app).post("/logout").send()
+      expect(res.status).toBe(401)
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.missingAuthHeader
+      )
     })
 
     test("should not logout twice with the same token", async () => {
@@ -137,12 +194,32 @@ describe("Auth routes", () => {
         .send()
 
       expect(res.status).toBe(401)
-      expect(res.body).toHaveProperty("error")
-      expect(res.body.error).toContain("revoked")
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.sessionRevoked
+      )
+    })
+
+    test("should not logout if refresh token is expired in db ", async () => {
+      const signinRes = await signin(testUserForLogout)
+      const accessToken = signinRes.body.accessToken
+
+      // Mock the database response to simulate expired session
+      mockExpiredRefreshedToken()
+      const res = await request(app)
+        .post("/logout")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send()
+
+      expect(res.status).toBe(401)
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.refreshTokenExpired
+      )
     })
   })
 
-  describe("Revocation - new_token", () => {
+  describe("New Token", () => {
     test("should refresh access token with valid refresh token", async () => {
       await signup(testUserForRevocation)
       const signinRes = await signin(testUserForRevocation)
@@ -156,8 +233,8 @@ describe("Auth routes", () => {
       expect(refreshRes.status).toBe(200)
       expect(refreshRes.body).toHaveProperty("accessToken")
       expect(refreshRes.body).toHaveProperty(
-        "message",
-        "Access token refreshed"
+        messageKey,
+        authMessages.success.accessTokenRefreshed
       )
       expect(refreshRes.headers["set-cookie"]).toBeDefined()
     })
@@ -165,39 +242,10 @@ describe("Auth routes", () => {
     test("should fail to refresh token if refresh token is missing", async () => {
       const res = await request(app).post("/signin/new_token").send()
       expect(res.status).toBe(400)
-      expect(res.body).toHaveProperty("error")
-    })
-
-    test("should fail to refresh token if refresh token is invalid", async () => {
-      await signin(testUserForRevocation)
-
-      const invalidCookie = [
-        "refreshToken=eyJhbGdiOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im5ldysxMjM0NTY3ODkwIiwic2Vzc2lvbklkIjoiYzFkNzQ2MjEtZGEzZS00ZjNmLWEyMWQtNGQ5MmE2MDViMWY2IiwiaWF0IjoxNzUzNzMwNTY4LCJleHAiOjE3NTQzMzUzNjh9.QcK91zovO53X4Jn3S7rdFsO7Oy0wjXSsGaIQNTDBkPw; Max-Age=604800; Path=/signin/new_token; Expires=Mon, 04 Aug 2025 19:22:48 GMT; HttpOnly; Secure; SameSite=Strict",
-      ]
-      const refreshRes = await request(app)
-        .post("/signin/new_token")
-        .set("Cookie", invalidCookie)
-        .send()
-
-      expect(refreshRes.status).toBe(500)
-      expect(refreshRes.body).toHaveProperty("error")
-      expect(refreshRes.body.error).toContain("nvalid")
-    })
-
-    test("should fail to refresh token if refresh token is malformed", async () => {
-      await signin(testUserForRevocation)
-      const malformedCookie = [
-        "refreshToken=malformed; Max-Age=604800; Path=/signin/new_token; Expires=Mon, 04 Aug 2025 19:22:48 GMT; HttpOnly; Secure; SameSite=Strict",
-      ]
-
-      const refreshRes = await request(app)
-        .post("/signin/new_token")
-        .set("Cookie", malformedCookie)
-        .send()
-
-      expect(refreshRes.status).toBe(500)
-      expect(refreshRes.body).toHaveProperty("error")
-      expect(refreshRes.body.error).toContain("malformed")
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.missingRefreshToken
+      )
     })
 
     test("should not refresh token if it was revoked by logout", async () => {
@@ -215,9 +263,60 @@ describe("Auth routes", () => {
         .set("Cookie", cookies)
         .send()
 
-      expect(res.status).toBe(500)
-      expect(res.body).toHaveProperty("error")
-      expect(res.body.error).toContain("revoked")
+      expect(res.status).toBe(401)
+      expect(res.body).toHaveProperty(
+        errorKey,
+        authMessages.error.invalidOrRevokedRefreshToken
+      )
+    })
+
+    test("should not refresh access token with valid refresh token which was expired in db", async () => {
+      const signinResToGetRefreshToken = await signin(testUserForRevocation)
+
+      const cookies = signinResToGetRefreshToken.headers["set-cookie"]
+
+      // Mock the database response to simulate an expired refresh token
+      mockExpiredRefreshedToken()
+
+      // Attempt to refresh the token with the "expired refresh" token
+      const refreshRes = await request(app)
+        .post("/signin/new_token")
+        .set("Cookie", cookies)
+        .send()
+
+      expect(refreshRes.status).toBe(401)
+      expect(refreshRes.body).toHaveProperty(
+        errorKey,
+        authMessages.error.refreshTokenExpired
+      )
+    })
+
+    test("should not refresh access token with valid refresh token which was vanished from db", async () => {
+      const signinRes = await signin(testUserForRevocation)
+
+      const cookies = signinRes.headers["set-cookie"]
+
+      // Mock the database response to simulate a vanished refresh token
+      function mockUpdateReturnsEmptyArray(target: any) {
+        target.update = () => mockReturnsEmptyArray()
+      }
+
+      vi.spyOn(db, "transaction").mockImplementation(async (cb) => {
+        const mockTx: any = {}
+        mockUpdateReturnsEmptyArray(mockTx)
+        return cb(mockTx)
+      })
+
+      const refreshRes = await request(app)
+        .post("/signin/new_token")
+        .set("Cookie", cookies)
+        .send()
+
+      expect(refreshRes.status).toBe(500)
+      expect(refreshRes.body).toHaveProperty(
+        errorKey,
+        authMessages.error.sessionNotFoundOrRevoked
+      )
     })
   })
 })
